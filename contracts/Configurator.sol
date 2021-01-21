155f4572f3415020120ae535b6b7dd40c184691c
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2021-01-21 17:59:53 +0300

    chore(Configurator): fix compiler errors, refactor

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -13,6 +13,41 @@ contract Configurator is RetrieveTokensFeature {
 
     uint256 private constant MAX = ~uint256(0);
 
+    uint256 private constant COMPANY_RESERVE_AMOUNT    = 31500000 * 1 ether;
+    uint256 private constant TEAM_AMOUNT               = 21500000 * 1 ether;
+    uint256 private constant SALE_AMOUNT               = uint256(143500000 * 1 ether * 100) / 98;
+
+    address private constant OWNER_ADDRESS             = address(0x0);
+    address private constant COMPANY_RESERVE_ADDRESS   = address(0x0);
+    address private constant LIQUIDITY_WALLET_ADDRESS  = address(0x0);
+    address payable constant ETH_WALLET_ADDRESS        = address(0x0);
+
+    uint256 private constant PRICE                   = 10000;            // 1 ETH = 10000 10SET
+
+    uint256 private constant STAGE1_START_DATE       = 1612072800;       // Jan 31 2021 07:00:00 GMT+0100
+    uint256 private constant STAGE1_END_DATE         = 1612677600;       // Feb 07 2021 07:00:00 GMT+0100
+    uint256 private constant STAGE1_BONUS            = 10;
+    uint256 private constant STAGE1_MIN_INVESTMENT   = 1 * 10 ** 17;     // 0.1 ETH
+    uint256 private constant STAGE1_MAX_INVESTMENT   = 40 * 1 ether;     // 40 ETH
+    uint256 private constant STAGE1_TOKEN_HARDCAP    = 11000000 * 1 ether;
+
+    uint256 private constant STAGE2_START_DATE       = 1612677600;       // Feb 07 2021 07:00:00 GMT+0100
+    uint256 private constant STAGE2_END_DATE         = 1613282400;       // Feb 14 2021 07:00:00 GMT+0100 
+    uint256 private constant STAGE2_BONUS            = 5;
+    uint256 private constant STAGE2_MIN_INVESTMENT   = 0.1 * 1 ether;    // 0.1 ETH
+    uint256 private constant STAGE2_MAX_INVESTMENT   = 100 * 1 ether;    // 100 ETH
+    uint256 private constant STAGE2_TOKEN_HARDCAP    = 52500000 * 1 ether;
+
+    uint256 private constant STAGE3_START_DATE       = 1613282400;       // Feb 14 2021 07:00:00 GMT+0100 
+    uint256 private constant STAGE3_END_DATE         = 253374588000;     // Feb 14 9999 07:00:00 GMT+0100 
+    uint256 private constant STAGE3_BONUS            = 0;
+    uint256 private constant STAGE3_MIN_INVESTMENT   = 0;                // 0 ETH
+    uint256 private constant STAGE3_MAX_INVESTMENT   = MAX;
+    uint256 private constant STAGE3_TOKEN_HARDCAP    = 80000000 * 1 ether;
+
+    address[] private addresses;
+    uint256[] private amounts;
+
     TenSetToken public token;
     FreezeTokenWallet public freezeWallet;
     CommonSale public commonSale;
@@ -21,18 +56,6 @@ contract Configurator is RetrieveTokensFeature {
         // create instances
         freezeWallet = new FreezeTokenWallet();
         commonSale = new CommonSale();
-
-        uint256 COMPANY_RESERVE_AMOUNT    = 31500000 * 1 ether;
-        uint256 TEAM_AMOUNT               = 21500000 * 1 ether;
-        uint256 SALE_AMOUNT               = (143500000 * 1 ether).mul(100).div(98);
-
-        address OWNER_ADDRESS             = address(0x0);
-        address COMPANY_RESERVE_ADDRESS   = address(0x0);
-        address LIQUIDITY_WALLET_ADDRESS  = address(0x0);
-        address ETH_WALLET_ADDRESS        = address(0x0);
-
-        address[] memory addresses;
-        uint256[] memory amounts;
 
         addresses.push(address(freezeWallet));
         amounts.push(TEAM_AMOUNT);
@@ -48,30 +71,8 @@ contract Configurator is RetrieveTokensFeature {
         commonSale.setToken(address(token));
         freezeWallet.setToken(address(token));
 
-        uint256 PRICE                   = 10000;            // 1 ETH = 10000 10SET
-        
-        uint256 STAGE1_START_DATE       = 1612072800;       // Jan 31 2021 07:00:00 GMT+0100
-        uint256 STAGE1_END_DATE         = 1612677600;       // Feb 07 2021 07:00:00 GMT+0100
-        uint256 STAGE1_BONUS            = 10;
-        uint256 STAGE1_MIN_INVESTMENT   = 1 * 10 ** 17;     // 0.1 ETH
-        uint256 STAGE1_MAX_INVESTMENT   = 40 * 1 ether;     // 40 ETH
-        uint256 STAGE1_TOKEN_HARDCAP    = 11000000 * 1 ether;
-
-        uint256 STAGE2_START_DATE       = 1612677600;       // Feb 07 2021 07:00:00 GMT+0100
-        uint256 STAGE2_END_DATE         = 1613282400;       // Feb 14 2021 07:00:00 GMT+0100 
-        uint256 STAGE2_BONUS            = 5;
-        uint256 STAGE2_MIN_INVESTMENT   = 0.1 * 1 ether;    // 0.1 ETH
-        uint256 STAGE2_MAX_INVESTMENT   = 100 * 1 ether;    // 100 ETH
-        uint256 STAGE2_TOKEN_HARDCAP    = 52500000 * 1 ether;
-
-        uint256 STAGE3_START_DATE       = 1613282400;       // Feb 14 2021 07:00:00 GMT+0100 
-        uint256 STAGE3_END_DATE         = 253374588000;     // Feb 14 9999 07:00:00 GMT+0100 
-        uint256 STAGE3_BONUS            = 0;
-        uint256 STAGE3_MIN_INVESTMENT   = 0;                // 0 ETH
-        uint256 STAGE3_MAX_INVESTMENT   = MAX;
-        uint256 STAGE3_TOKEN_HARDCAP    = 80000000 * 1 ether;
-
         commonSale = new CommonSale();
+
         commonSale.setPrice(PRICE);
         commonSale.setWallet(ETH_WALLET_ADDRESS);
         commonSale.addMilestone(STAGE1_START_DATE, STAGE1_END_DATE, STAGE1_BONUS, STAGE1_MIN_INVESTMENT, STAGE1_MAX_INVESTMENT, 0, 0, STAGE1_TOKEN_HARDCAP);
@@ -81,7 +82,7 @@ contract Configurator is RetrieveTokensFeature {
         commonSale.addMilestone(STAGE3_START_DATE, STAGE3_END_DATE, STAGE3_BONUS, STAGE3_MIN_INVESTMENT, STAGE3_MAX_INVESTMENT, 0, 0, STAGE3_TOKEN_HARDCAP);
 
         freezeWallet.start();
-        
+
         token.transferOwnership(OWNER_ADDRESS);
         freezeWallet.transferOwnership(OWNER_ADDRESS);
         commonSale.transferOwnership(OWNER_ADDRESS);
