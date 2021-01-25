dd8be9fa6d220008f44ea6c665ee73686d69c899
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2021-01-25 18:20:31 +0300

    chore(test): add tests for FreezeWallet

--- test/configurator.test.js
+++ test/configurator.test.js
@@ -48,6 +48,7 @@ describe('Configurator', async function () {
     this.commonSale = await CommonSale.at(this.commonSaleAddress);
     
     await web3.eth.sendTransaction({ from: donor, to: OWNER_ADDRESS, value: ether("10")})
+    await web3.eth.sendTransaction({ from: donor, to: TEAM_WALLET_OWNER_ADDRESS, value: ether("10")})
   });
   
   describe('CommonSale', function() {
@@ -92,6 +93,10 @@ describe('Configurator', async function () {
 
     it('should not allow non-owners to change ETH wallet', async function () {
       await expectRevert(this.commonSale.setWallet(account1, {from: account1}), "Ownable: caller is not the owner");
+    });
+
+    it('should not allow non-owners to change price', async function () {
+      await expectRevert(this.commonSale.setPrice(ether('1000'), {from: account1}), "Ownable: caller is not the owner");
     });
   })
 
@@ -219,6 +224,62 @@ describe('Configurator', async function () {
       expect(await this.freezeWallet.owner()).to.equal(TEAM_WALLET_OWNER_ADDRESS);
     });
 
+    it('should not allow premature withdrawal of tokens', async function () {
+      await this.freezeWallet.retrieveWalletTokens(account1, {from: TEAM_WALLET_OWNER_ADDRESS});
+      const balance = await this.token.balanceOf(account1);
+      expect(balance).to.be.bignumber.equal(new BN('0'));
+    });
+
+    it('should not allow to withdraw TesSet tokens with retriveTokens method', async function () {
+      await expectRevert.unspecified(this.freezeWallet.retrieveTokens(TEAM_WALLET_OWNER_ADDRESS, this.tokenAddress, {from: TEAM_WALLET_OWNER_ADDRESS}));
+    });
+
+    it('should allow to retrieve tokens other than TenSetToken', async function () {
+      const alienToken = await ERC20Mock.new('TEST', 'TST', account1, new BN(1000), {from: account1});
+      await alienToken.transfer(this.freezeWalletAddress, new BN(1000), {from: account1});
+      const {receipt: {transactionHash}} = await this.freezeWallet.retrieveTokens(account1, alienToken.address, {from: TEAM_WALLET_OWNER_ADDRESS});
+      const events = await getEvents(transactionHash, alienToken,'Transfer', web3);
+      const amountToTransfer = events[0].args.value;
+      expect(new BN(amountToTransfer)).to.be.bignumber.equal(new BN(1000));
+    });
+ 
+    it('should allow withdrawal of tokens in accordance with the withdrawal policy', async function () {
+      await this.freezeWallet.retrieveWalletTokens(account1, {from: TEAM_WALLET_OWNER_ADDRESS});
+      const initialBalance = await this.token.balanceOf(this.freezeWalletAddress);
+      const tranche = initialBalance.mul(new BN(98)).div(new BN(1000)); // 98% of (initialBalance / 10)
+      const months = n => n * 30 * 24 * 3600;
+      const tenths = n => initialBalance.mul(new BN(`${n}`)).div(new BN(10));
+      const intervals = [
+        {idx: 0, delay: months(3), tranche, remainder: tenths(9)},
+        {idx: 1, delay: months(6), tranche, remainder: tenths(8)},
+        {idx: 2, delay: months(9), tranche, remainder: tenths(7)},
+        {idx: 3, delay: months(12), tranche, remainder: tenths(6)},
+        {idx: 4, delay: months(15), tranche, remainder: tenths(5)},
+        {idx: 5, delay: months(18), tranche, remainder: tenths(4)},
+        {idx: 6, delay: months(21), tranche, remainder: tenths(3)},
+        {idx: 7, delay: months(24), tranche, remainder: tenths(2)},
+        {idx: 8, delay: months(27), tranche, remainder: tenths(1)},
+        {idx: 9, delay: months(30), tranche, remainder: tenths(0)},
+      ];
+      for (const {idx, delay, tranche, remainder} of intervals) {
+        const currentDate = await time.latest();
+        if (currentDate < STAGE1_START_DATE + delay) await time.increaseTo(STAGE1_START_DATE + delay);
+        const {receipt: {transactionHash}} = await this.freezeWallet.retrieveWalletTokens(account1, {from: TEAM_WALLET_OWNER_ADDRESS});
+        const events = await getEvents(transactionHash, this.token,'Transfer', web3);
+        // sometimes fails and requires rerunning the test
+        const tokensToSend = new BN(events[1].args.value);
+        const tokensRemained = await this.token.balanceOf(this.freezeWalletAddress);
+        if (idx !== 9) {
+          expect(tokensToSend).to.be.bignumber.equal(tranche);
+          expect(tokensRemained).to.be.bignumber.gte(remainder);
+        } else {
+          // the last withdrawal should send all remained tokens
+          // the amount to send should be slightly larger as RFI increases the balance with every transaction
+          expect(tokensToSend).to.be.bignumber.gte(tranche);
+          expect(tokensRemained).to.be.bignumber.equal(new BN('0'));
+        }
+      }
+    });
   })
  
 });
